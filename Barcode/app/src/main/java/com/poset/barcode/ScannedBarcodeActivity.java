9f51d00dcfc3d04cd46cd289cb578c09f40868fe
Burak Buldu <buldubu@gmail.com>
2020-01-12 15:56:57 +0300

    theme

--- Barcode/app/src/main/java/com/poset/barcode/ScannedBarcodeActivity.java
+++ Barcode/app/src/main/java/com/poset/barcode/ScannedBarcodeActivity.java
@@ -19,6 +19,7 @@ import com.google.android.gms.vision.barcode.Barcode;
 import com.google.android.gms.vision.barcode.BarcodeDetector;
 
 import java.io.BufferedReader;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
@@ -57,6 +58,7 @@ public class ScannedBarcodeActivity extends AppCompatActivity {
             @Override
             protected void onPostExecute(String s) {
                 super.onPostExecute(s);
+
                 if(s != null && !s.equals("rro")) {
                     String[] datas = s.split("\",\"");
                     for (int i = 0; i < datas.length; i++) {
@@ -66,12 +68,68 @@ public class ScannedBarcodeActivity extends AppCompatActivity {
                         else if(i == datas.length-1) map.put("\""+keyValue[0], keyValue[1]);
                         else map.put("\""+keyValue[0], keyValue[1]+"\"");
                     }
+                    //System.out.println(map);
+
+
+                    /*
                     txtBarcodeValue.post(new Runnable() {
                         @Override
                         public void run() {
                             txtBarcodeValue.setText("Barcode: " + map.get("\"barkod\"") + "\n" + "Name: " + map.get("\"urun_adi\""));
                         }
                     });
+                     */
+
+                    int i = 0;
+                    String son = "";
+                    String comp = "\"1\"";
+                    try {
+                        FileInputStream fIn = openFileInput ( "settings.dat" ) ;
+                        InputStreamReader isr = new InputStreamReader ( fIn ) ;
+                        BufferedReader buffreader = new BufferedReader ( isr ) ;
+                        String readString = buffreader.readLine ( ) ;
+                        while ( readString != null ) {
+                            if(i == 0 && map.get("\"vegan\"").equals(comp) && Boolean.parseBoolean(readString) == true){
+                                son = son + "Veganlara uygun degil!\n";
+                                //System.out.println(i);
+                            }
+                            else if(i == 1 && map.get("\"vejeteryan\"").equals(comp) && Boolean.parseBoolean(readString) == true){
+                                son = son + "Vejeteryanlara uygun degil!\n";
+                                //System.out.println(i);
+                            }
+                            else if(i == 2 && map.get("\"non_pork\"").equals(comp) && Boolean.parseBoolean(readString) == true){
+                                son = son + "Domuz ve urunlerı icerir!\n";
+                                //System.out.println(i);
+                            }
+                            else if(i == 3 && map.get("\"gluten\"").equals(comp) && Boolean.parseBoolean(readString) == true){
+                                son = son + "Gluten icerir!\n";
+                                //System.out.println(i);
+                            }
+                            else if(i == 4 && map.get("\"fruktoz\"").equals(comp) && Boolean.parseBoolean(readString) == true){
+                                son = son + "Fruktoz icerir!\n";
+                                //System.out.println(i);
+                            }
+                            else if(i == 5 && map.get("\"yer_fistigi\"").equals(comp) && Boolean.parseBoolean(readString) == true) {
+                                son = son + "Yer fistigi icerir!\n";
+                                //System.out.println(i);
+                            }
+                            i++;
+                            readString = buffreader.readLine ( ) ;
+                        }
+                        //System.out.println(son);
+                        final String ans = son;
+                        txtBarcodeValue.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                txtBarcodeValue.setText("Name: " + map.get("\"urun_adi\"") +  "\n" + ans);
+                            }
+                        });
+
+                        isr.close ( ) ;
+                    } catch ( IOException ioe ) {
+                        System.out.println("Hata = " + ioe);
+                        ioe.printStackTrace ( ) ;
+                    }
                 }
             }
             @Override
@@ -145,7 +203,7 @@ public class ScannedBarcodeActivity extends AppCompatActivity {
             public void receiveDetections(Detector.Detections<Barcode> detections) {
                 final SparseArray<Barcode> barcodes = detections.getDetectedItems();
                 date2 = new Date();
-                if (barcodes.size() != 0 && ((int) (date2.getTime() - date1.getTime()))/100 > 5) {
+                if (barcodes.size() != 0 && ((int) (date2.getTime() - date1.getTime()))/100 > 10) {
                     date1 = new Date();
                     intentData = barcodes.valueAt(0).displayValue;
                     downloadJSON("http://172.24.5.51:8080/deneme/index.php?barkod=" + intentData);
